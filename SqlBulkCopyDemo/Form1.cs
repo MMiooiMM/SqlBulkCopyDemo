3b1e2d5565b3b9e4e82faeee3ac7a1a8e6e65179
mio <mio093611@gmail.com>
2019-05-09 17:30:09 +0800

    =ˇ=

--- SqlBulkCopyDemo/Form1.cs
+++ SqlBulkCopyDemo/Form1.cs
@@ -15,6 +15,59 @@ namespace SqlBulkCopyDemo
         public Form1()
         {
             InitializeComponent();
+            this.Height = 320;
+        }
+
+        public int status { get; set; } = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlBulkCopyDemo sqlBulk = new SqlBulkCopyDemo(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+            switch (status)
+            {
+                case 0:
+                    sqlBulk.Copy();
+                    break;
+                case 1:
+                    sqlBulk.Copy(
+                        this.checkBox1.Checked, 
+                        Convert.ToInt32(this.textBox5.Text), 
+                        Convert.ToInt32(this.textBox6.Text)
+                        );
+                    break;
+                case 2:
+                    sqlBulk.Copy(
+                       this.checkBox1.Checked,
+                       Convert.ToInt32(this.textBox5.Text),
+                       Convert.ToInt32(this.textBox6.Text),
+                       this.textBox7.Text,
+                       this.textBox8.Text
+                       );
+                    break;
+            }            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            status = 1;
+            this.button2.Visible = false;
+            this.checkBox1.Visible = true;
+            this.label5.Visible = true;
+            this.textBox5.Visible = true;
+            this.label6.Visible = true;
+            this.textBox6.Visible = true;
+            this.Height += 50;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            status = 2;
+            this.button3.Visible = false;
+            this.label7.Visible = true;
+            this.textBox7.Visible = true;
+            this.label8.Visible = true;
+            this.textBox8.Visible = true;
+            this.Height += 50;
         }
     }
 }
---
+++ SqlBulkCopyDemo/SqlBulkCopyDemo.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SqlBulkCopyDemo
+{
+    public class SqlBulkCopyDemo
+    {
+        public string SourceConnectionString { get; set; }
+        public string DestinationConnectionString { get; set; }
+        public string SourceTableName { get; set; }
+        public string DestinationTableName { get; set; }
+
+        public SqlBulkCopyDemo(string sourceConnectionString, string destinationConnectionString, string sourceTableName, string destinationTableName)
+        {
+            SourceConnectionString = sourceConnectionString;
+            DestinationConnectionString = destinationConnectionString;
+            SourceTableName = sourceTableName;
+            DestinationTableName = destinationTableName;
+        }
+        public void Copy()
+        {
+            using (SqlConnection sourceConnection = new SqlConnection(SourceConnectionString))
+            {
+                sourceConnection.Open();
+
+                Print("CopyTable start");
+
+                SqlCommand commandSourceData = new SqlCommand($"SELECT * FROM {SourceTableName};", sourceConnection);
+                SqlDataReader reader = commandSourceData.ExecuteReader();
+                
+                using (SqlConnection destinationConnection = new SqlConnection(DestinationConnectionString))
+                {
+                    destinationConnection.Open();
+
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(destinationConnection))
+                    {
+                        bulkCopy.BatchSize = 100000; // 一次批次寫入多少筆資料
+                        bulkCopy.NotifyAfter = 100000; // 在寫入多少筆資料後呼叫 SqlRowsCopied 事件
+                        bulkCopy.SqlRowsCopied += OnSqlRowsCopied;
+                        bulkCopy.BulkCopyTimeout = 1800; // 逾時秒數
+                        bulkCopy.DestinationTableName = DestinationTableName; // 要寫入的資料表名稱
+                        try
+                        {
+                            bulkCopy.WriteToServer(reader);
+                        }
+                        catch (Exception ex)
+                        {
+                            Print(ex.Message);
+                        }
+                        finally
+                        {
+                            reader.Close();
+                            Print("CopyTable finish");
+                        }
+                    }                    
+                }
+            }
+        }
+
+        public void Copy(bool truncate, int batchSize, int timeout)
+        {
+            using (SqlConnection sourceConnection = new SqlConnection(SourceConnectionString))
+            {
+                sourceConnection.Open();
+
+                Print("CopyTable start");
+
+                SqlCommand commandSourceData = new SqlCommand($"SELECT * FROM {SourceTableName};", sourceConnection);
+                SqlDataReader reader = commandSourceData.ExecuteReader();
+
+                using (SqlConnection destinationConnection = new SqlConnection(DestinationConnectionString))
+                {
+                    destinationConnection.Open();
+
+                    if (truncate)
+                    {
+                        Print($"truncate table {DestinationTableName}");
+                        SqlCommand truncateDestinationData = new SqlCommand($"truncate table {DestinationTableName}", destinationConnection);
+                        truncateDestinationData.ExecuteNonQuery();
+                    }
+
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(destinationConnection))
+                    {
+                        bulkCopy.BatchSize = batchSize; // 一次批次寫入多少筆資料
+                        bulkCopy.NotifyAfter = batchSize; // 在寫入多少筆資料後呼叫 SqlRowsCopied 事件
+                        bulkCopy.SqlRowsCopied += OnSqlRowsCopied;
+                        bulkCopy.BulkCopyTimeout = timeout; // 逾時秒數
+                        bulkCopy.DestinationTableName = DestinationTableName; // 要寫入的資料表名稱
+                        try
+                        {
+                            bulkCopy.WriteToServer(reader);
+                        }
+                        catch (Exception ex)
+                        {
+                            Print(ex.Message);
+                        }
+                        finally
+                        {
+                            reader.Close();
+                            Print("CopyTable finish");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Copy(bool truncate, int batchSize, int timeout, string sourceColumns, string destinationColumns)
+        {
+            var sourceColumnArray = sourceColumns.Split(';');
+            var destinationColumnArray = destinationColumns.Split(';');
+            if(sourceColumnArray.Count() != destinationColumnArray.Count())
+            {
+                Print("sourceColumns not equal to destinationColumns");
+                return;
+            }
+            using (SqlConnection sourceConnection = new SqlConnection(SourceConnectionString))
+            {
+                sourceConnection.Open();
+
+                Print("CopyTable start");
+
+                SqlCommand commandSourceData = new SqlCommand($"SELECT {string.Join(",", sourceColumnArray)} FROM {SourceTableName};", sourceConnection);
+                SqlDataReader reader = commandSourceData.ExecuteReader();
+
+                using (SqlConnection destinationConnection = new SqlConnection(DestinationConnectionString))
+                {
+                    destinationConnection.Open();
+
+                    if (truncate)
+                    {
+                        Print($"truncate table {DestinationTableName}");
+                        SqlCommand truncateDestinationData = new SqlCommand($"truncate table {DestinationTableName}", destinationConnection);
+                        truncateDestinationData.ExecuteNonQuery();
+                    }
+
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(destinationConnection))
+                    {
+                        bulkCopy.BatchSize = batchSize; // 一次批次寫入多少筆資料
+                        bulkCopy.NotifyAfter = batchSize; // 在寫入多少筆資料後呼叫 SqlRowsCopied 事件
+                        bulkCopy.SqlRowsCopied += OnSqlRowsCopied;
+                        bulkCopy.BulkCopyTimeout = timeout; // 逾時秒數
+                        bulkCopy.DestinationTableName = DestinationTableName; // 要寫入的資料表名稱
+                        for(int i=0;i< sourceColumnArray.Count(); i++)
+                        {
+                            bulkCopy.ColumnMappings.Add(sourceColumnArray[i], destinationColumnArray[i]);
+                        }
+                        try
+                        {
+                            bulkCopy.WriteToServer(reader);
+                        }
+                        catch (Exception ex)
+                        {
+                            Print(ex.Message);
+                        }
+                        finally
+                        {
+                            reader.Close();
+                            Print("CopyTable finish");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnSqlRowsCopied(Object sender, SqlRowsCopiedEventArgs args)
+        {
+            Print($"{args.RowsCopied.ToString()} rows are copied.");
+        }
+
+        private void Print(string message)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss")} {message}");
+        }
+    }
+}
